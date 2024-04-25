35f95cca238221b91b900324485175f3f359a6b5
shinebox <96853660+Loon15@users.noreply.github.com>
2024-04-24 21:09:34 -0600

    Added If statements so console returned rock, paper, scissors instead of numbers.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function getComputerChoice() {
-    let rock = "ROCK";                              //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”. 
+    let rock = "ROCK";                            //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”. 
     let paper = "PAPER";
     let scissors = "SCISSORS";
-    let getRandom = Math.random();
-    return getRandom;                              //outputs random number using getRandom
+    let getRandom = Math.random();               //outputs random number using getRandom                          
+//console.log(getComputerChoice());  
+    if(getRandom <= 0.33) {                      // random number from 3 / 1 then 3/2 and 3/3
+    return rock;
+
+}  else if(getRandom <= 0.66) {
+    return paper;
+
+}  else{
+    return scissors;
 }
-//console.log(getComputerChoice());   
+ 
+} //console.log(getComputerChoice());            //outputs rock, paper or scissors to console
+
+
+             
