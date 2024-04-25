8505a561084e9a0a240e0fde650182dd9d903823
shinebox <96853660+Loon15@users.noreply.github.com>
2024-04-24 20:51:44 -0600

    Created getComputerChoice function adding rock, paper, scissors. Successfully added random number generator.

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let rock = "ROCK";                              //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”. 
+    let paper = "PAPER";
+    let scissors = "SCISSORS";
+    let getRandom = Math.random();
+    return getRandom;                              //outputs random number using getRandom
+}
+//console.log(getComputerChoice());   
